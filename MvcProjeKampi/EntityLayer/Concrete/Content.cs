548dbad7fb4009cdf82950c93dbd2eff15d4168d
antcel07 <93091784+antcel07@users.noreply.github.com>
2022-07-27 19:39:44 +0300

    Mvc Proje Kampi Görev1
    Murat Yücedağ ile youtube üzerinden proje kampında kendimi geliştiriyorum

---
+++ MvcProjeKampi/EntityLayer/Concrete/Content.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+   public class Content
+    {
+        //burada 5 başlık vardır bunlara veri girişi yaparız
+       [Key]
+        public int ContentId { get; set; }
+        [StringLength(1000)]
+        public string ContentValue { get; set; }
+        public DateTime ContentDate { get; set; }
+
+        //heading ile ilişki kuruldu id üzerinden
+        public int HeadingId { get; set; }
+        public virtual Heading Heading { get; set; }
+        //yazarın ıd ile bağlantı kuruldu 
+
+       public int? WriterId { get; set; }
+       public virtual Writer Writer { get; set; }
+
+    }
+}
